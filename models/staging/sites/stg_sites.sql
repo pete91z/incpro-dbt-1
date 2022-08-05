d6a7348536ab5e3380048e800490613380a870ef
peter.carpenter@blaqfire.com <peter.carpenter@blaqfire.com>
2022-08-05 11:17:32 +0000

    changed stg_sites

--- models/staging/sites/stg_sites.sql
+++ models/staging/sites/stg_sites.sql
@@ -10,12 +10,16 @@ with sites as (
     select site_loc_id as site_id,
        site_name,
        site_desc as site_classification,
-       postcode,
-       split_part(postcode,' ',1) as postcode_outcode,
-       split_part(postcode,' ',2) as postcode_incode,
-       split_part(postcode,' ',1)||' '||substring(split_part(postcode,' ',2),1,1) as sector,
-       regexp_substr(split_part(postcode,' ',2),'[A-Z]{2}') as unit,
-       substring(postcode,1,2) as postcode_area, lat,long,date_added,last_update_date
+       coalesce(postcode,'UNKNOWN') as postcode,
+       coalesce(split_part(postcode,' ',1),'UNKNOWN') as postcode_outcode,
+       coalesce(split_part(postcode,' ',2),'UNKNOWN') as postcode_incode,
+       coalesce(split_part(postcode,' ',1)||' '||substring(split_part(postcode,' ',2),1,1),'UNKNOWN') as sector,
+       coalesce(regexp_substr(split_part(postcode,' ',2),'[A-Z]{2}'),'UNKNOWN') as unit,
+       coalesce(substring(postcode,1,2),'UNKNOWN') as postcode_area, 
+       lat,
+       long,
+       date_added,
+       last_update_date
 from {{ source('sites', 'sites') }}
 {% if is_incremental() %}
     where last_update_date::timestamp > (select fetch_timestamp::timestamp from {{ source('control', 'incr_control') }} where model_name='{{ this.identifier }}')
