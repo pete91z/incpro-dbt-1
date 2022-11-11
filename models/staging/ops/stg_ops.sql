2bd6235e7e75f5cdda6e4d1323865ae0f936f3a8
peter.carpenter@blaqfire.com <peter.carpenter@blaqfire.com>
2022-11-11 15:19:22 +0000

    Updated stg_ops to include attributes from hq_locations, person, org_unit and role

--- models/staging/ops/stg_ops.sql
+++ models/staging/ops/stg_ops.sql
@@ -6,13 +6,20 @@
 }}
 
 with ops as (
-    select operator_id,first_name,last_name,position,status,startdate,email,phone,base,last_update_date
+    select operator_id,first_name||' '||last_name operator_name,position,status,startdate,email,phone,base,last_update_date
 from {{ source('ops', 'operators') }}
 {% if is_incremental() %}
     where last_update_date::timestamp > (select fetch_timestamp::timestamp from {{ source('control', 'incr_control') }} where model_name='{{ this.identifier }}')
 {% endif %}
 )
 
-select a.*,b.role_description from ops a
-join {{source('ops','role')}} b 
-on a.position=b.role_id
+select op.*,
+       ro.role_description,  
+       pe.first_names||' '||pe.last_name as manager_name,
+       ou.unit_name as department,
+       hq.location_name
+from ops op
+left join {{source('ops','role')}} ro on (op.position=ro.role_id)
+left join {{source('ops','hq_locations')}} hq on (op.base=hq.location_code)
+left join {{source('ops','org_unit')}} ou on (lower(substring(ro.role_description,1,4))=lower(substring(ou.unit_name,1,4))) and (hq.location_id=ou.hq_id)
+left join {{source('ops','person')}} pe on (ou.manager_id=pe.id)
